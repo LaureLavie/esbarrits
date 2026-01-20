ba6005d27bc8ed988c66db2a1861aac76af72dd1
BenSTAU <ben.saint.augustin@gmail.com>
2026-01-20 14:23:14 +0100

    navbar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -43,7 +43,7 @@ export default function Navbar() {
                 <a href="/contact" className={pathname === '/contact' ? "active" : ""}>Contact</a>
             </div>
 
-            {/* Burger Menu Button */}
+            {/* Burger Menu Butto */}
             <div className='burger'>
 
                 <button

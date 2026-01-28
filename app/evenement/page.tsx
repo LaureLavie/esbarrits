bafd029ed92589ff32bc10e66a7abc8ce4a16e25
LaureLavie <laure.lavie@gmail.com>
2026-01-28 10:26:39 +0100

    event festival 2023

--- app/evenement/page.tsx
+++ app/evenement/page.tsx
@@ -6,11 +6,13 @@ import Image from 'next/image';
 import '../../styles/components/evenement.css';
 import sample1 from '../../public/images/sample1.jpg';
 import sample2 from '../../public/images/sample2.jpg';
+import sample3 from '../../public/images/sample3.jpg';
 
 export default function EventPage() {
   const items = [
     { id: 1, title: 'Portes Ouvertes du Jurançon - 2025', excerpt: 'Esbarrits aux Portes ouvertes décembre 2025', img: sample1, link: 'https://photos.app.goo.gl/dCQxYKieb6aZ9QN2A' },
     { id: 2, title: 'Soirée Chataîgnes & Bourret - 2023', excerpt: 'Retour en images sur notre animation', img: sample2, link: 'https://photos.app.goo.gl/MJZomYkwfWzW4ndDA' },
+    { id: 3, title: 'Festival des Vallées - 2023', excerpt: 'Retour en images sur notre prestation à Oloron', img: sample3, link: 'https://photos.app.goo.gl/DgKc7xEzp9xUfX2T8' },
   ];
 
   return (
